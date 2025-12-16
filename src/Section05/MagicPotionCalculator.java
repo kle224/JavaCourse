9d0a4cda104ddecf94206c04320a5fd8637e2330
Noah <noahklein432+github@gmail.com>
2025-12-16 16:05:33 +0100

    Created MainSection05 and MagicPotionCalculator

---
+++ src/Section05/MagicPotionCalculator.java
@@ -0,0 +1,10 @@
+package Section05;
+
+public class MagicPotionCalculator {
+	public static void main(String []args) {
+		int magicEssence = 5, dragonScale = 3, unicornHair = 2;
+		int potionPower = magicEssence * dragonScale + unicornHair;
+		
+		System.out.println("The power of the mixture is: " + potionPower);
+	}
+}
---
+++ src/Section05/MainSection05.java
@@ -0,0 +1,5 @@
+package Section05;
+
+public class MainSection05 {
+
+}
