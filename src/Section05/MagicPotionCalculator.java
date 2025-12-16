667752edfe8cb93fae8911e806461988786eac88
Noah <noahklein432+github@gmail.com>
2025-12-16 16:26:02 +0100

    Added a Line in MagicPotionCalc

--- src/Section05/MagicPotionCalculator.java
+++ src/Section05/MagicPotionCalculator.java
@@ -5,6 +5,8 @@ public class MagicPotionCalculator {
 		int magicEssence = 5, dragonScale = 3, unicornHair = 2;
 		int potionPower = magicEssence * dragonScale + unicornHair;
 		
+		System.out.println(String.format("Testing with String.format: %d", potionPower));
+		
 		System.out.println("The power of the mixture is: " + potionPower);
 	}
 }
