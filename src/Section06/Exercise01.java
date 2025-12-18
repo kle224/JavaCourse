227561a2bcac997daca7cdbddbd6b570212a6d37
Noah <noahklein432+github@gmail.com>
2025-12-18 16:02:12 +0100

    added a new package for section 6

---
+++ src/Section06/Exercise01.java
@@ -0,0 +1,7 @@
+package Section06;
+
+public class Exercise01 {
+	public void run() {
+		System.out.println("123");
+	}
+}
---
+++ src/Section06/MainSection06.java
@@ -0,0 +1,8 @@
+package Section06;
+
+public class MainSection06 {
+	public static void main(String []args) {
+		Exercise01 task01 = new Exercise01();
+		task01.run();
+	}
+}
