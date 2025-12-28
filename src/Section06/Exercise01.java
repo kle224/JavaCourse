bc37a8448d162a46b9ec1936a83842f48cba3e21
Noah <noahklein432+github@gmail.com>
2025-12-28 12:18:09 +0100

    coded a small example for refactoring into S06 E01

--- src/Section06/Exercise01.java
+++ src/Section06/Exercise01.java
@@ -7,8 +7,11 @@ public class Exercise01 {
 		int one = 1, two = 2, three = 3;
 		System.out.println("Initialized the variables \"one\", \"two\""
 						  + "and \"three\"");
-		System.out.printf("one: %d\n", one);
-		System.out.printf("two: %d\n", two);
-		System.out.printf("three: %d\n", three);
+		System.out.printf("one: %d%n", one);
+		System.out.printf("two: %d%n", two);
+		System.out.printf("three: %d%n", three);
+		
+		System.out.println("Refactoring \"three\" into \"x\", using the shortcut:");
+		System.out.printf("x: %d%n", three);
 	}
 }
