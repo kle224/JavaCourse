0b82988e6af5aec32d10605d321da47d6c36e084
Noah <noahklein432+github@gmail.com>
2025-12-28 12:57:57 +0100

    added tasks file (two tasks included), coded an example for ternary statements in S06 E01

--- src/Section06/Exercise01.java
+++ src/Section06/Exercise01.java
@@ -1,6 +1,26 @@
 package Section06;
 
+import java.lang.classfile.constantpool.DoubleEntry;
+import java.util.Scanner;
+
 public class Exercise01 {
 	public void run() {
+		System.out.println("Example for the ternary statement:");
+		
+		int age;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("The discount depends on your age. Children below 13 years will pay less.");
+		
+		System.out.println("Enter your age: ");
+		age = scan.nextInt();
+
+		// Ternary guide:
+		// type name = condition ? if true : if false;
+		double discount = (age <= 12) ? 0.10 : 0.05;
+		System.out.printf("age: %d years.%n", age);
+		
+		
+		System.out.printf("Discount for age %d is %.2f%%. %n", age, (discount * 100));
 	}
 }
