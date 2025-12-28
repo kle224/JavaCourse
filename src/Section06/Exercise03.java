8c34db041ba522fe91025874dc1d86292f1d45ad
Noah <noahklein432+github@gmail.com>
2025-12-28 13:42:00 +0100

    completed the Code in S06 E03 for nested if-else-statements

---
+++ src/Section06/Exercise03.java
@@ -0,0 +1,19 @@
+package Section06;
+
+public class Exercise03 {
+	public static void run() {
+		int a = 5, b = 10;
+		
+		// Example for nested if-else-statements
+		
+		if (a > 0) {
+			if (b > a) {
+				System.out.println("B is greater than A!");
+			} else if (b == a) {
+				System.out.println("B is equal to A!");
+			} else {
+				System.out.println("B is lesser than A!");
+			}
+		}
+	}
+}
