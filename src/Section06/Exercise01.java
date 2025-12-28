5467c766cacc8393dcbf32cd625ba621b2416c8c
Noah <noahklein432+github@gmail.com>
2025-12-28 12:10:22 +0100

    modified sof-template-line in readme and added lines into S06 E01

--- src/Section06/Exercise01.java
+++ src/Section06/Exercise01.java
@@ -5,6 +5,10 @@ public class Exercise01 {
 		System.out.println("Shortcut for refactoring is \"ALT + SHIFT + R\".");
 		
 		int one = 1, two = 2, three = 3;
-		System.out.printf("one: %d", one);
+		System.out.println("Initialized the variables \"one\", \"two\""
+						  + "and \"three\"");
+		System.out.printf("one: %d\n", one);
+		System.out.printf("two: %d\n", two);
+		System.out.printf("three: %d\n", three);
 	}
 }
