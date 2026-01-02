e567a0f86ef394b8798018b79a33522aeeaa1590
Noah <noahklein432+github@gmail.com>
2026-01-02 22:28:21 +0100

    commented S08 E02 for better understanding

--- src/Section08/Exercise02.java
+++ src/Section08/Exercise02.java
@@ -1,6 +1,7 @@
 package Section08;
 
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
 
 public class Exercise02 {
 	public static void run() {
@@ -12,11 +13,23 @@ public class Exercise02 {
 		for (int i = 1; i <= levels; i++) {
 			
 			// print spaces
+//			Beispiel nach Reihen:
+//			1. Condition wird vier Mal erfüllt
+//			2. Condition wird drei Mal erfüllt
+//			3. Condition wird zwei Mal erfüllt
+//			4. Condition wird ein Mal erfüllt
+//			5. Condition wird null Mal erfüllt
 			for (int spaces = 0; spaces < (levels - i); spaces++) {
 				System.out.print(" ");
 			}
 			
 			// print stars
+//			Beispiel nach Reihen:
+//				1. Condition wird ein Mal erfüllt
+//				2. Condition wird drei Mal erfüllt
+//				3. Condition wird fünf Mal erfüllt
+//				4. Condition wird sieben Mal erfüllt
+//				5. Condition wird neun Mal erfüllt
 			for (int stars = 0; stars < (2 * i) - 1; stars++) {
 				System.out.print("*");
 			}
