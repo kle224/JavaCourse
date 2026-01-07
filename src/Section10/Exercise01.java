f795609620738e411309527f97b09656d57f88d0
Noah <noahklein432+github@gmail.com>
2026-01-07 10:58:24 +0100

    started coding in S10 E01

--- src/Section10/Exercise01.java
+++ src/Section10/Exercise01.java
@@ -2,6 +2,17 @@ package Section10;
 
 public class Exercise01 {
 	public static void run() {
+		// declaring a 3x3 array
+		int[][] nums1 = new int[3][3];
 		
+		// declaring and initializing a 3x3 array with it's values
+		int[][] nums2 = { {1,2,3}, {4,5,6}, {7,8,9}, {1,2,3,4} };
+		
+		for (int i = 0; i < nums2.length; i++) {
+			for (int j = 0; j < nums2[i].length; j++) {
+				System.out.print(nums2[i][j] + " ");
+			}
+			System.out.println("");
+		}
 	}
 }
--- src/Section10/MainSection10.java
+++ src/Section10/MainSection10.java
@@ -1,5 +1,25 @@
 package Section10;
 
 public class MainSection10 {
-
+	public static void main (String []argStrings) {
+		// Section Info:: Working with 2D Arrays
+		
+		Exercise01 task01 = new Exercise01();
+		task01.run();
+		
+//		Exercise02 task02 = new Exercise02();
+//		task02.run();
+		
+//		Exercise03 task03 = new Exercise03();
+//		task03.run();
+		
+//		Exercise04 task04 = new Exercise04();
+//		task04.run();
+		
+//		Exercise05 task05 = new Exercise05();
+//		task05.run();
+		
+//		Exercise06 task06 = new Exercise06();
+//		task06.run();
+	}
 }
