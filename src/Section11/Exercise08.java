5f3914cbb53a6878d827bb792e6849d63acedb43
Noah <noahklein432+github@gmail.com>
2026-01-16 23:36:58 +0100

    started coding S11 E08

--- src/Section11/Exercise08.java
+++ src/Section11/Exercise08.java
@@ -1,6 +1,34 @@
 package Section11;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Exercise08 {
 	public static void run() {
+		Scanner scan = new Scanner(System.in);
+		Random rnd = new Random();
+		
+		System.out.print("Please enter your email: ");
+		isValidMail(scan.nextLine());
+	}
+	
+	public static boolean isValidMail(String mail) {
+		char[] elements = mail.toCharArray();
+		
+		searchCharacter(elements, '@');
+		
+		return true;
+	}
+	
+	public static void searchCharacter(char[] characters, char searched) {
+		int searchedCounter = 0;
+		
+		for (char part : characters) {
+			if (part == searched) {
+				searchedCounter++;
+			}
+		}
+		System.out.println("Looking for @: " + Arrays.binarySearch(characters, searched));
 	}
 }
--- src/Section11/MainSection11.java
+++ src/Section11/MainSection11.java
@@ -33,14 +33,14 @@ public class MainSection11 {
 //		Exercise06 task06 = new Exercise06();
 //		task06.run();
 
-		System.out.println("7:");
-		// - 
-		Exercise07 task07 = new Exercise07();
-		task07.run();
+//		System.out.println("7:");
+//		// - scramble game
+//		Exercise07 task07 = new Exercise07();
+//		task07.run();
 
-//		System.out.println("8:");
-//		// - 
-//		Exercise08 task08 = new Exercise08();
-//		task08.run();
+		System.out.println("8:");
+		// - validate an email address
+		Exercise08 task08 = new Exercise08();
+		task08.run();
 	}
 }
